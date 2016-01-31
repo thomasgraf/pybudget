dc4ca0c2a490912b01c7b22053d16814ed1cd50c
tgraf <thomas.graf@ise.fraunhofer.de>
2016-01-31 14:13:24 +0100

    added schema for add form for expenses

--- pybudget/schema.py
+++ pybudget/schema.py
@@ -2,23 +2,44 @@ from colander import MappingSchema
 from colander import SchemaNode
 from colander import String
 from colander import Integer
-from colander import DateTime
+from colander import Decimal
+from colander import Date
 from colander import deferred
+import deform
+
 
 class ExpenseSchema(MappingSchema):
     """Schema for adding and editing expenses to the database."""
-    name = SchemaNode(String())
-    tags = SchemaNode(
-        String(),
-        widget=TagsWidget(
-            autocomplete_url='/tags.autocomplete',
-        ),
-        description=(
-            "Enter a comma after each tag to add it. Backspace to delete."
-        ),
-        missing=[],
+    account = SchemaNode(
+        colander.String(),
+        widget=deform.widget.SelectWidget(
+        values=(
+            ('', '-- Wählen --'),
+            ('bar', 'Bar'),
+            ('ec', 'EC-Karte'),
+            ('visa', 'VISA'),
+            ('mastercard','MAstercard')
+        ))
     )
-    due_date = SchemaNode(
-        deferred_datetime_node,
-        missing=None,
+    expense_date = SchemaNode(
+        Date(),
+    )
+    amount = SchemaNode(
+        Decimal(),
+        widget = deform.widget.MoneyInputWidget(
+            size=20,
+            options={'allowZero':True}
+        )
+    )
+
+    category = SchemaNode(
+        colander.String(),
+        widget=deform.widget.SelectWidget(
+        values=(
+            ('', '-- Wählen --'),
+            ('lebensmittel', 'Lebensmittel'),
+            ('hyg_haus', 'Hygiene/Haushalt'),
+            ('kleidung', 'Kleidung'),
+            ('sonstiges','Sonstiges')
+        ))
     )
